8f81578c0ca24da876708cba0690d6af4bbdbc74
sanehakaur77 <snehaswali9988@gmail.com>
2025-07-16 17:27:45 +0530

    Delete Day13 directory

--- Day13/backend/models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-const Userschema=new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String
-})
-const User=mongoose.model('User',Userschema);
-module.exports=User;
--- Day13/backend/routes/routes.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const {SignUpValidation,LoginValidation}=require('../middlewares/middleware');
-const {signUp,login} =require('../controllers/controller');
-router.post('/signUp',SignUpValidation,signUp);
-router.post('/login',LoginValidation,login);
-module.exports=router;
